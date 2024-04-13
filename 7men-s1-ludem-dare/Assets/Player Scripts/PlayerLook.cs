b68cfb36c395ee3f41be09c01049de001ed84fd6
JackFellow <144679743+JackFellow@users.noreply.github.com>
2024-04-14 00:29:29 +0200

    Better Movement

--- 7men-s1-ludem-dare/Assets/Player Scripts/PlayerLook.cs
+++ 7men-s1-ludem-dare/Assets/Player Scripts/PlayerLook.cs
@@ -1,12 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+
 public class PlayerLook : MonoBehaviour
 {
     public Transform player;
 
     Camera mainCam;
+
+    public NavMeshAgent agent;
+
+    public Vector3 CameraPos;
+
+    private PlayerInputActions inputActions;
+
+    private InputAction fire;
+
+    private InputAction dash;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        dash = inputActions.Player.Dash;
+        dash.Enable();
+        dash.performed += Dash;
+
+        fire = inputActions.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+
+    private void OnDisable()
+    {
+        dash.Disable();
+
+        fire.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,20 +53,46 @@ public class PlayerLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveCamera();
     }
 
     void FixedUpdate()
     {
-        //Create Ray
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
         RaycastHit hit;
 
-        Ray mousePos = mainCam.ScreenPointToRay(Input.mousePosition);
+        //casts ray to mous position
+        Ray mousePos = mainCam.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-        if (Physics.Raycast(mousePos,out hit, 100f))
+        //if there was a ray cast the player will look at it
+        if (Mouse.current.leftButton.isPressed && Physics.Raycast(mousePos, out hit, 100f))
         {
-            Vector3 lookAtPos = new Vector3(hit.point.x, player.position.y, hit.point.z);
-            player.LookAt(lookAtPos);
+            player.LookAt(new Vector3(hit.point.x, 0f, hit.point.z));
         }
+
+        //if there was a ray cast  and the player pressed the left mouse button the player's navmesh agent's destination will be set to it
+        if (Mouse.current.leftButton.isPressed && Physics.Raycast(mousePos, out hit, 100f))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+
+    void MoveCamera()
+    {
+        mainCam.transform.position = new Vector3(player.transform.position.x + CameraPos.x, player.transform.position.y + CameraPos.y, player.transform.position.z + CameraPos.z);
+    }
+
+    private void Dash(InputAction.CallbackContext context)
+    {
+        Debug.Log("dash");
+    }
+
+    private void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("fire");
     }
 }
