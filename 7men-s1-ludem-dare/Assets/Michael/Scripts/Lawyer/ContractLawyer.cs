7da9b83de202f38a9870885bcccf84e2c033d91c
mdhenry763 <mdhenry763@gmail.com>
2024-04-15 10:55:58 +0200

    Lawyer Handling system almost complete.

---
+++ 7men-s1-ludem-dare/Assets/Michael/Scripts/Lawyer/ContractLawyer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Managers.Lawyer
+{
+    public class ContractLawyer : Lawyer
+    {
+        
+        public override void OnSpawn()
+        {
+            base.OnSpawn();
+        }
+
+        public override void OnAttack()
+        {
+            base.OnAttack();
+            Debug.Log("Contract Attacking");
+        }
+
+        public override void OnDeath()
+        {
+            Debug.Log("Contract Death");
+            base.OnDeath();
+            
+        }
+    }
+}
---
+++ 7men-s1-ludem-dare/Assets/Michael/Scripts/Lawyer/ProsecutorLawyer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Managers.Lawyer
+{
+    public class ProsecutorLawyer : Lawyer
+    {
+        public override void OnSpawn()
+        {
+            base.OnSpawn();
+        }
+
+        public override void OnAttack()
+        {
+            base.OnAttack();
+        }
+
+        public override void OnDeath()
+        {
+            base.OnDeath();
+        }
+    }
+}
